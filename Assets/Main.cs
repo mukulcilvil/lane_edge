f1050c0b8fc391a28d9e1ff2b5865f51ac97bb54
mukulcivil <mukul@civilmaps.com>
2018-10-17 17:32:21 +0530

    dummu hello world

---
+++ Assets/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	void onGUI (){
+		GUILayout.Label ("Hello World");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World!");
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
